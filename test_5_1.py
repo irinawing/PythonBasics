e3a08f2deb145576254f53c6c900d91c7caa8251
irinawing <irina.wing@mail.ru>
2025-05-27 22:47:15 +0300

    Урок 4. Множества и отображения (словари)

--- test_5_1.py
+++ test_5_1.py
@@ -13,7 +13,7 @@
 original_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 
 # Список уникальных элементов — тех, что встречаются только один раз
-unique_elements = [x for x in original_list if original_list.count(x) == 1]
+unique_elements = [elem for elem in original_list if original_list.count(elem) == 1]
 
 # Вывод результата
 print("Результат:", unique_elements)
